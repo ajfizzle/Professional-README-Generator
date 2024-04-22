828ad19e45dee33f95723393346bb6f3feac5b92
Joel Azeta <ajfizzle310@outlook.com>
2024-04-21 20:54:40 -0500

    Learn To Code

--- Develop/index.js
+++ Develop/index.js
@@ -1,102 +1,95 @@
-const inquirer = require("inquirer");
-const generateMarkdown = require('./utils/generateMarkdown');
-const fs = require("fs");
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
-function writeToFile(fileName, data) {
-  const fileText = generateMarkdown(data);
-
-  fs.writeFile(fileName, fileText, (err) => {
-    if (err) {
-      console.error("Error writing file:", err);
-    } else {
-      console.log("README file generated successfully!");
-    }
-  });
-}
-
-// function tiggers the application and generates user questions using inquirer
-function promptUser() {
-  return inquirer.prompt([
-    // User name input
+// Array of questions for user input
+const questions = [
     {
-      type: "input",
-      message: "What is your name?",
-      name: "name",
-    },
-    // User README title input
-    {
-      type: "input",
-      message: "What is the title of your project?",
-      name: "title",
-    },
-    // User README description input
-    {
-      type: "input",
-      message: "Description of your project:",
-      name: "description",
-    },
-    // User README installation instructions input
-    {
-      type: "input",
-      message: "Instruction(s) on how to install your project?",
-      name: "installation",
-    },
-    // User README usage information input
-    {
-      type: "input",
-      message: "How would usage information of your project:",
-      name: "usage",
-    },
-    // User README test information input
-    {
-      type: "input",
-      message: "How would test your project:",
-      name: "test",
-    },
-    // User README licensing options
-    {
-      type: "list",
-      message: "Which license will you use for your project?",
-      choices: [
-        "Apache 2.0 License",
-        "GNU 3.0 License",
-        "MIT",
-        "BSD 2-Clause",
-        "BSD 3-Clause",
-        "The Unlicense",
-      ],
-      name: "license",
-    },
-    // User github handle input
-    {
-      type: "input",
-      message: "What is your GitHub Username?",
-      name: "github",
-    },
-    // User email handle input
-    {
-      type: "input",
-      message: "What is your email?",
-      name: "email",
-    },
-       // User suggestion/comment handle input
-       {
         type: "input",
-        message: "Do you have any suggestion(s) to the project?",
-        name: "suggestions",
-      },
-  ]);
+        message: "What is your name?",
+        name: "name",
+    },
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Description of your project:",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Instruction(s) on how to install your project?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "How would usage information of your project:",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "We are open to any contribution(s) to this project:",
+        name: "contributions",
+    },
+    {
+        type: "input",
+        message: "How would test your project:",
+        name: "test",
+    },
+    {
+        type: "list",
+        message: "Which license will you use for your project?",
+        choices: [
+            "Apache License 2.0",
+            "GNU General Public License v3.0",
+            "MIT",
+            "BSD 2-Clause",
+            "BSD 3-Clause",
+            "The Unlicense",
+        ],
+        name: "license",
+    },
+    {
+        type: "input",
+        message: "What is your GitHub Username?",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+    },
+];
+
+// Function to prompt questions and store user inputs
+const promptUser = () => {
+    return inquirer.prompt(questions);
 }
 
-// function to write README file
-function init() {
-  try {
-    promptUser().then((data) => {
-      writeToFile("generated-README.md", data);
+// Function to write README file
+const writeToFile = (fileName, data) => {
+    const markdown = generateMarkdown(data);
+    fs.writeFile(fileName, markdown, (err) => {
+        if (err) {
+            console.error("Error writing file:", err);
+        } else {
+            console.log("README file generated successfully!");
+        }
     });
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
+}
+
+// Function to initialize app
+const init = () => {
+    promptUser()
+        .then(data => {
+            writeToFile("generated-README.md", data);
+        })
+        .catch(error => {
+            console.error("An error occurred:", error);
+        });
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,72 +1,59 @@
-// Function to render the license badge based on the license type
-function renderLicenseBadge(license) {
-    if (license !== 'no license') {
-      return `[${license}](https://choosealicense.com/licenses/${license})`;
-    } else {
-      return '';
-    }
-  }
-  
-  // Function to render the license link
-  function renderLicenseLink(license) {
-    if (license !== 'no license') {
-      return `
-  ## License
-  
-  The application is covered under the following license:
-  
-  ${renderLicenseBadge(license)}
-  `;
-    } else {
-      return '';
-    }
-  }
-  
-  // Function to render the license section of README
-  function renderLicenseSection(license) {
-    if (license !== 'no license') {
-      return `* [License](#license)`;
-    } else {
-      return '';
-    }
-  }
-  
-  // Function to generate markdown for README
-  function generateMarkdown(data) {
-    return `# ${data.title}
-  
-  ${renderLicenseBadge(data.license)}
-  
-  ## Table of Contents
-  
-  - [Description](#description)
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [Test Instructions](#test-instructions)
-  - [License](#license)
-  - [Suggestions](#suggestions)
-  
-  ## Description
-  ${data.description}
-  
-  ## Installation
-  ${data.installation}
-  
-  ## Usage
-  ${data.usage}
-  
-  ## Test Instructions
-  ${data.test}
-  
-  ## License
-  ${(data.license)}
-  
-  ## Suggestions
-  ${(data.suggestions)}
-  
-  For questions about the project, contact [${data.name}](https://github.com/${data.github}) at ${data.email}.
-  `;
-  }
-  
-  module.exports = generateMarkdown;
-  
+function generateLicenseBadge(license) {
+  const licenseBadgeLinks = {
+      "Apache License 2.0": "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+      "GNU General Public License v3.0": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+      "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+      "BSD 2-Clause": "[![License: BSD-2-Clause](https://img.shields.io/badge/License-BSD--2--Clause-blue.svg)](https://opensource.org/licenses/BSD-2-Clause)",
+      "BSD 3-Clause": "[![License: BSD-3-Clause](https://img.shields.io/badge/License-BSD--3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+      "The Unlicense": "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+  };
+
+  return licenseBadgeLinks[license] || "";
+}
+
+function generateMarkdown(data) {
+  return `
+
+# ${data.title} by ${data.name}
+
+${generateLicenseBadge(data.license)}
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+
+## Description
+
+${data.description}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributions
+
+${data.contributions}
+
+## Test Instructions
+
+${data.test}
+
+## License
+
+This project is licensed under the ${data.license} license. See the [LICENSE](LICENSE) file for details.
+
+## Questions
+
+For questions about the project, contact [${data.name}](https://github.com/${data.github}) at ${data.email}.
+`;
+}
+
+module.exports = generateMarkdown;
